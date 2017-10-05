bf3fe2d78a7df4575e946fa6b08880d401e4e79e
Mariley20 <mariley2018@gmail.com>
2017-10-05 15:57:33 -0500

    cambiar de boton parar  iniciar

--- app.jsx
+++ app.jsx
@@ -4,8 +4,9 @@ class Model {
       constructor(players) {
             this.index = 0;
             this.players = players;
-            this.callback = null;
             this.input = null;
+            this.button = false;
+            this.callback = null;
       }
 
       subscribe(render) {
@@ -15,34 +16,39 @@ class Model {
       notify() {
             this.callback();
       }
-      totalPuntos () {
+      totalPuntos() {
             let total = 0;
-            for(let i = 0; i < this.players.length; i++){
-              total = total + this.players[i].score;
-          } 
-          return total;
+            for (let i = 0; i < this.players.length; i++) {
+                  total = total + this.players[i].score;
+            }
+            return total;
       }
-      totalJugadores (){
+      totalJugadores() {
             return this.players.length;
       }
-      agregarJugador (nombre) {
+      agregarJugador(nombre) {
             console.log(nombre);
             this.players.push({
                   name: nombre.value,
                   score: 0,
                   id: Utils.uuid()
-               });
-               this.notify();
+            });
+            this.notify();
       }
       modificarScore(e, score, index, x) {
             this.players[index].score = score + x;
-            this.notify() ;
+            this.notify();
+      }
+      iniciaPara(e) {
+            console.log(Date.now());
+           this.button = (this.button)? false : true;
+           this.notify();
       }
 }
 
 const Application = ({ title, model }) => {
       console.log(model.players)
-          let header = (<div className="row">
+      let header = (<div className="row">
             <div className="col col-xl-8">
                   <div className="row">
                         <div className="col col-xl-3 col-sm-3">
@@ -58,29 +64,33 @@ const Application = ({ title, model }) => {
             <div className="col col-xl-4 col-sm-4">
                   <p>StopWath</p>
                   <p>0</p>
-                  <button type="">start</button>
+                  {model.button ?
+                        <button onClick={e => model.iniciaPara(e)}>Stop</button>
+                        :
+                        <button onClick={e => model.iniciaPara(e)}>Start</button>
+                  }
                   <button>reset</button>
             </div>
       </div>);
       let playerList = model.players.map((elemento, index) => {
             return (<div className="row text-center" key={index}>
                   <div className="col col-xl-8 col-sm-8">{elemento.name}</div>
-                  <div className="col col-xl-1 col-sm-1 "><button onClick={e => {model.modificarScore(e, elemento.score, index, -1)}}> - </button></div>
+                  <div className="col col-xl-1 col-sm-1 "><button onClick={e => { model.modificarScore(e, elemento.score, index, -1) }}> - </button></div>
                   <div className="col col-xl-2 col-sm-2">{elemento.score}</div>
-                  <div className="col col-xl-1 col-sm-1"><button onClick={e => {model.modificarScore(e, elemento.score, index, 1)}}> + </button></div>
+                  <div className="col col-xl-1 col-sm-1"><button onClick={e => { model.modificarScore(e, elemento.score, index, 1) }}> + </button></div>
             </div>);
       });
       let playerForm = (<form className="add-player-form" onSubmit={e => {
             e.preventDefault();
             model.agregarJugador(model.input);
-         }}><div className="row">
-            <div className="col col-xl-8 col-sm-8">
-                  <input type="text" onChange={e => (model.input = e.target)} className="form-control" placeholder="enter a name" /></div>
-            <div className="col col-xl-4 col-sm-4"><button type="submit">Add Player</button></div>
-      </div></form>);
+      }}><div className="row">
+                  <div className="col col-xl-8 col-sm-8">
+                        <input type="text" onChange={e => (model.input = e.target)} className="form-control" placeholder="enter a name" /></div>
+                  <div className="col col-xl-4 col-sm-4"><button type="submit">Add Player</button></div>
+            </div></form>);
       return (<div className="scoreboard">
             {header}
-           {playerList}
+            {playerList}
             {playerForm}
       </div>
       );
